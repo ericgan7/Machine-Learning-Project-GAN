2049364756b79e12ac4e043b6803db61a387188e
Eric Gan <eg2584@nyu.edu>
2019-04-30 00:47:56 -0400

    binary classification bug
    used binary classification instad of categorical classification. scaling data is very important as well.

--- NerualNet.py
+++ NerualNet.py
@@ -4,12 +4,20 @@ from keras.layers import Flatten, Input, Dense, Dropout, BatchNormalization, Lam
 from keras.layers.advanced_activations import LeakyReLU
 from keras.regularizers import l2
 from keras.optimizers import Adam, SGD, RMSprop, Nadam
+from keras.callbacks import Callback
 
 import sys
 import numpy as np
 import random
 import os
 import matplotlib.pyplot as plt
+
+class AccuracyHistory(Callback):
+    def on_train_begin(self,logs={}):
+        self.acc = []
+
+    def on_batch_end(self,batch, logs={}):
+        self.acc.append(logs.get('acc'))
 
 class NerualNetwork():
     def __init__(self):
@@ -20,18 +28,16 @@ class NerualNetwork():
         self.trainLabel = []
         self.testData = []
         self.testLabel = []
-        self.opt = Adam(0.0002, 0.5, clipnorm=1)
-
+        self.opt = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
     
         self.build_model()
     
     def build_model(self):
         self.model = Sequential([
-            Dense(500, input_dim = self.input_dimension),
-            LeakyReLU(alpha=0.2),
-            Dense(self.output_dimension, activation = 'sigmoid')
+            Dense(128, input_dim = self.input_dimension, activation = 'relu'),
+            Dense(self.output_dimension, activation = 'softmax')
         ])
-        self.model.compile(optimizer = self.opt, loss = 'binary_crossentropy', metrics=['accuracy'])
+        self.model.compile(optimizer = 'adam', loss = "sparse_categorical_crossentropy", metrics=['accuracy'])
 
     def load_data(self, train, test):
         with open(train, 'r') as f:
@@ -45,10 +51,10 @@ class NerualNetwork():
                     continue
                 image = line.split(',')
                 data.append(list(map(int,image[1:])))
-                lab = [0 for i in range(10)]
-                lab[int(image[0])] = 1
-                label.append(lab)
-            self.trainData = np.array(data)
+                #lab = [0 for i in range(10)]
+                #lab[int(image[0])] = 1
+                label.append(image[0])
+            self.trainData = np.array(data) / 255
             self.trainLabel = np.array(label)
         with open(test, 'r') as f:
             print("Loading {}".format(test))
@@ -61,21 +67,30 @@ class NerualNetwork():
                     continue
                 image = line.split(',')
                 data.append(list(map(int, image[1:])))
-                lab = [0 for i in range(10)]
-                lab[int(image[0])] = 1
-                label.append(lab)
-            self.testData = np.array(data)
+                #lab = [0 for i in range(10)]
+                #lab[int(image[0])] = 1
+                label.append(image[0])
+            self.testData = np.array(data) / 255
             self.testLabel = np.array(label)
-        print(self.trainData.shape)
-        print(self.trainLabel.shape)
         print("Done Loading Data")
 
     def train(self, epochs = 100, batch_size = 32):
         print("Begin Training")
-        self.model.fit(self.trainData, self.trainLabel, epochs = epochs, batch_size = batch_size)
-        score = self.model.evaluate(self.testData, self.testLabel, batch_size = batch_size)
+        history = AccuracyHistory()
+        self.model.fit(self.trainData, self.trainLabel, epochs = epochs, batch_size = batch_size, callbacks=[history])
+        self.plot(history.acc)
+        score = self.model.evaluate(self.testData, self.testLabel)
         print(score)
 
+    def plot(self, acc):
+        plt.plot(range(len(acc)), acc, label="Model")
+        plt.xlabel('Batch')
+        plt.ylabel('Accuracy')
+        plt.legend()
+        plt.title('Classifier')
+        plt.grid(True)
+        plt.show()
+        
 """
     def load_data(self, train, test):
         self.load(train[0], self.input_dimension, self.trainData)
@@ -96,7 +111,7 @@ class NerualNetwork():
 """
 if __name__ == '__main__':
     use_saved = sys.argv[1] if len(sys.argv) > 1 else None
-    nnclassifier = NerualNetwork(use_saved)
+    nnclassifier = NerualNetwork()
     #nnclassifier.load_data(("trainimages", "trainlabels"), ("testimages", "testlabels"))
     nnclassifier.load_data("data/train.csv", "data/test.csv")
-    nnclassifier.train(epochs=10, batch_size=32)
+    nnclassifier.train(epochs=10, batch_size=100)
